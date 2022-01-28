79657c08d9e1839a83eb82eae0aa2b7fe3c9a3d3
Michael Buffone <m_buffone@hotmail.com>
2022-01-28 10:20:24 -0500

    initial commit

---
+++ eqArrays.js
@@ -0,0 +1,22 @@
+//function implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); //  false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // true 
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // false
