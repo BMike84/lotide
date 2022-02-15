2fcd6ef15f7fdc9bdc89295feade319f1d6a1a70
Michael Buffone <m_buffone@hotmail.com>
2022-02-15 18:53:55 -0500

    added mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,18 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-//test code
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(tail(words).length, 3);
+describe("#tail", () => {
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+  it("returns 3 for words.length", () => {
+    assert.strictEqual((words.length), 3);
+  });
+  
+  it("returns 2 for tail(words).length", () => {
+    assert.strictEqual(tail(words).length, 2);
+  });
+
+  it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5,]), [2, 3, 4, 5,]);
+  });
+
+})
