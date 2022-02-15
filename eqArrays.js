8c739fb0dc9a8719c24456d42b3009025e92919d
Michael Buffone <m_buffone@hotmail.com>
2022-02-15 12:38:54 -0500

    refactor remove assertEquals

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,4 @@
-//function implementation
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 function eqArrays(one, two) {
   if(one.length !== two.length) {
@@ -19,7 +12,4 @@ function eqArrays(one, two) {
   return true;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); //  false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // true 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // false
+module.exports = eqArrays;
