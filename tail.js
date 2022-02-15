d3cf13f6973e98e387194a4bea1e254b54d23cb0
Michael Buffone <m_buffone@hotmail.com>
2022-02-15 12:33:29 -0500

    refactor to remove assertequal

--- tail.js
+++ tail.js
@@ -1,17 +1,13 @@
+const assertEqual = require('./assertEqual');
+
 const tail = function(words) {
   const newArray = words.slice(1);
   return newArray;
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+module.exports = tail;
 
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(tail(words).length, 3);
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(tail(words).length, 3);
