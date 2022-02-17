f389b55a0abccc7d1b4b4a06c1aba3df0fffe924
Michael Buffone <m_buffone@hotmail.com>
2022-02-16 19:46:59 -0500

    removed test code

--- head.js
+++ head.js
@@ -4,8 +4,4 @@ const head = function(actual) {
   return actual[0];
 };
 
-// //test code
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
 module.exports = head;
