4bc116410a74913077cc5e4914031eba2bfb9659
Michael Buffone <m_buffone@hotmail.com>
2022-02-18 12:20:57 -0500

    fix error then delte

--- takeUntil.js
+++ takeUntil.js
@@ -42,11 +42,9 @@ const takeUntil = function(array, callback) {
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 assertArraysEqual(results1, [1, 2, 5, 7, 2]);
-
+//
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-<<<<<<< HEAD
 assertArraysEqual(results2, ["I've", 'been', 'to', 'Hollywood']);
-=======
 assertArraysEqual(results2, ["I've", 'been', 'to', 'Hollywood']);
->>>>>>> ca476235e962cfc0b35471a1e8797d73b5206fd6
+
