a7bffc5fc7c3f68981f10f3d89d37be36d1e47f8
Michael Buffone <m_buffone@hotmail.com>
2022-02-18 13:26:38 -0500

    refactored and added module.exports

--- takeUntil.js
+++ takeUntil.js
@@ -1,28 +1,5 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Actual Results: ${actual} === Expected Results: ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Actual Results: ${actual} !== Expected Results: ${expected}`);
-  }  
-};
-
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
-        if (!eqArrays(array1[i], array2[i])) {
-          return false;
-        }
-      } else if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
 
 const takeUntil = function(array, callback) {
   const results = [];
@@ -37,7 +14,7 @@ const takeUntil = function(array, callback) {
   return results;
 }
 
-
+module.exports = takeUntil;
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
