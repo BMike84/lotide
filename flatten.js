8b820bddf0b67c75fb3336acefe14bdb325fed49
Michael Buffone <m_buffone@hotmail.com>
2022-02-18 13:21:07 -0500

    refactored and added module.exports

--- flatten.js
+++ flatten.js
@@ -1,36 +1,13 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Actual Results: ${actual} === Expected Results: ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Actual Results: ${actual} !== Expected Results: ${expected}`);
-  }  
-};
-
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
-        if (!eqArrays(array1[i], array2[i])) {
-          return false;
-        }
-      } else if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
 
 const flatten = function(array) {
   let newArray = array.flat();
   return newArray;
 };
 
-<<<<<<< HEAD
+module.exports = flatten;
+
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
-=======
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
->>>>>>> ca476235e962cfc0b35471a1e8797d73b5206fd6
+
