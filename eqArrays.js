7a49daf25e310b374475e0fd7580272aa7a33ad2
Michael Buffone <m_buffone@hotmail.com>
2022-02-18 11:09:00 -0500

    fixed a bug for returning wrong expected

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,20 @@
 const assertEqual = require('./assertEqual');
 
-function eqArrays(one, two) {
-  if(one.length !== two.length) {
+function eqArrays(array1, array2) {
+  if (array1.length !== array2.length) {
     return false;
-  }
-  for (let i = 0; i < one.length; i++) {
-    if(one[i] !== two[i]) {
-      return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+        if (!eqArrays(array1[i], array2[i])) {
+          return false;
+        }
+      } else if (array1[i] !== array2[i]) {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
 }
 
 module.exports = eqArrays;
