3667e573a123e50f66cf7cf15a26de012bd5f435
Michael Buffone <m_buffone@hotmail.com>
2022-02-18 13:14:45 -0500

    refactor and added module.export

--- countOnly.js
+++ countOnly.js
@@ -1,11 +1,4 @@
-//function implementation
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 // allItems: an array of strings that we need to look through
 // itemsCount: an object specifying what to count
@@ -25,6 +18,8 @@ const countOnly = function(allItems, itemsToCount) {
 
   return results;
 }
+
+module.exports = countOnly;
 
 
 const firstNames = [
