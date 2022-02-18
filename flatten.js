cb340db65e62a184970f5564d34d6800a6870fe6
Michael Buffone <93268351+BMike84@users.noreply.github.com>
2022-02-18 11:48:43 -0500

    changed flatten function and changed console.log

--- flatten.js
+++ flatten.js
@@ -1,34 +1,32 @@
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
-    console.log(`${actual} === ${expected}`);
+    console.log(`✅✅✅ Actual Results: ${actual} === Expected Results: ${expected}`);
   } else {
-    console.log(`[${actual}] !== ${expected}`);
+    console.log(`🛑🛑🛑 Actual Results: ${actual} !== Expected Results: ${expected}`);
   }  
 };
 
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
     return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+        if (!eqArrays(array1[i], array2[i])) {
+          return false;
+        }
+      } else if (array1[i] !== array2[i]) {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
-};
+}
 
 const flatten = function(array) {
-  //quicker way using flat() to concat array.
-  // let newArray = array.flat();
-  // return newArray;
-
-  //using for loop and concat
-  let newArray = [];
-  for (let i = 0; i < array.length; i++) {
-    newArray = newArray.concat(array[i]);
-  }
+  let newArray = array.flat();
   return newArray;
 };
 
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
