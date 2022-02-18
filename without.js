2c58638ce1278838129e1e149d5857dffc764ce4
Michael Buffone <m_buffone@hotmail.com>
2022-02-18 12:33:01 -0500

    refactor with chai

--- without.js
+++ without.js
@@ -1,22 +1,6 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Actual Results: ${actual} === Expected Results: ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Actual Results: ${actual} !== Expected Results: ${expected}`);
-  }  
-};
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
 
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
 const without = function(array, value) {
   let resultsArray = [];
@@ -28,12 +12,4 @@ const without = function(array, value) {
   return resultsArray;
 };
 
-//test
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); 
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-assertArraysEqual(without(["1", "1", "1", "2", "3", "1" , "1"], ["1", 2, "3"]))
-console.log(without([1, 2, 3], [1]));
-console.log(without(["1", "2", "3"], [1, 2, "3"])); 
-console.log(without(["1", "1", "1", "2", "3", "1" , "1"], ["1", 2, "3"]));
-
+module.exports = without;
