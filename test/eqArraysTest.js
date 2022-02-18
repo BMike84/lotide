0c3844a6b634e3d22d8126f7be4c7e7fbea0ed5d
Michael Buffone <m_buffone@hotmail.com>
2022-02-18 11:10:27 -0500

    changed file to require chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,26 @@
-const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const eqArrays = require("../eqArrays");
 
-//test case
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); //  false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // true 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // false
+describe("#eqArrays", () => {
+  it("should return true for ([1, 2, 3], [1, 2, 3])", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("should return false for ([1, 2, 3], [3, 2, 1])", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+  it("should return true for (['1', '2', '3'], ['1', '2', '3'])", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+  it("should return false for (['1', '2', '3'], ['1', '2', 3])", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+  it("should return true for [[2, 3], [4]], [[2, 3], [4]])", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  });
+  it("should return false for ([[2, 3], [4]], [[2, 3], [4, 5])", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+  });
+  it("should return false for ([[2, 3], [4]], [[2, 3], 4])", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+  });
+});
