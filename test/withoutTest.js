0f450343821439f21dba207a0cfbde2f3d15b301
Michael Buffone <m_buffone@hotmail.com>
2022-02-18 14:03:06 -0500

    added a test case suggested in my evaluation

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -5,11 +5,17 @@ describe ("#without", () => {
   it("should return [2, 3] for ([1, 2, 3], [1])", () => {
     assert.deepEqual(without([1, 2, 3], [1]), [2, 3]);
   });
+  
   it("should return ['1', '2'] for (['1', '2', '3'], [1, 2, '3'])", () => {
     assert.deepEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
   });
-  const words = ["hello", "world", "lighthouse"];
+  
+  const words = ['hello', 'world', 'lighthouse'];
   it("should return [ 'hello', 'world' ] for (words, ['lighthouse'])", () => {
     assert.deepEqual(without(words, [ "lighthouse" ]), [ "hello", "world" ]);
+  });
+  
+  it("should return ['2'] for (['1', '1', '1', '2', '3', '1', '1'], ['1', 2, '3'])", () => {
+    assert.deepEqual(without(['1', '1', '1', '2', '3', '1', '1'], ['1', 2, '3']), ['2']);
   });
 });
