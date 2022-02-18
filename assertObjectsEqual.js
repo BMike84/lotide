f4ebe6b12a8df3686eab69e8b0f9f5f2e146ec01
Michael Buffone <m_buffone@hotmail.com>
2022-02-18 13:10:12 -0500

    added require to clean up code

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,40 +1,5 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
-        if (!eqArrays(array1[i], array2[i])) {
-          return false;
-        }
-      } else if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
-
-const eqObjects = function(object1, object2) {
-  const keyOne = Object.keys(object1);
-  const keyTwo = Object.keys(object2);
-
-  if (keyOne.length !== keyTwo.length) {
-    return false;
-  }
-
-  for (let key of keyOne) {
-    if(Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if(!eqArrays(object1[key], object2[key])) {
-        return false;
-      }
-    } else if (object1[key] !== object2[key]) {
-      return false
-    }
-    
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
