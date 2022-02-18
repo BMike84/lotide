7108737bed022603ac413a8aa2026074f5d790c7
Michael Buffone <m_buffone@hotmail.com>
2022-02-18 13:23:14 -0500

    refactored and added module.exports

--- letterPositions.js
+++ letterPositions.js
@@ -1,28 +1,5 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Actual Results: ${actual} === Expected Results: ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Actual Results: ${actual} !== Expected Results: ${expected}`);
-  }  
-};
-
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
-        if (!eqArrays(array1[i], array2[i])) {
-          return false;
-        }
-      } else if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
 
 const letterPositions = function (sentence) {
   const results = {};
@@ -39,8 +16,8 @@ const letterPositions = function (sentence) {
   return results;
 }
 
-<<<<<<< HEAD
+module.exports = letterPositions;
+
 assertArraysEqual(letterPositions("hello").o, [4]);
-=======
 assertArraysEqual(letterPositions("hello").o, [4]);
->>>>>>> ca476235e962cfc0b35471a1e8797d73b5206fd6
+
